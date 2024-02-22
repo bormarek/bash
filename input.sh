50e472d89313335c1dfdd86192a9948ea550044b
Marek Borkowski <marek@laptop-Marek.lan>
2024-02-22 22:15:03 +0100

    first commit

---
+++ input.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+DATE=`date | awk '{print $5}'`
+read -p "Enter your name: " name
+
+read -p "Enter your birth date: " year
+
+echo "Hello $name"
+
+echo $DATE
+age=$((DATE-year))
+
+if [[ "$name" = "marek" || "$name" = "Marek" ]]; then
+	echo "Perfect that you are here... $name it is very sad that you are $age years old"
+else
+	echo "hello stranger"
+fi
