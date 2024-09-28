2171f8f27e363361daeb9ae84c9a3a298594f33e
Marek Borkowski <bormarek@gmail.com>
2024-09-28 15:59:01 +0200

    commit

--- userParam.sh
+++ userParam.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ $# -lt 3 ]; then
+	echo "Error: not enough parameters entered."
+	echo "first param is name, second is home directory, third is favourite color"
+	exit 1
+fi
+
 echo "My name is $1"
 echo "My home directory is $2"
 echo "my favourite color is $3"
